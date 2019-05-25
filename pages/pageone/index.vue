53838ff83c1d07f65b2b20ecc91bdfd2786cc0a9
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2019-05-25 10:58:01 -0500

    FINALLLLLY got auto code fix/formating on save working.
    Steps:
    
    - Do not install prettier extension.
    - Install ESLint extension (fck).
    - Look at settings in .vscode/settings.json for fix on save functionality.

--- pages/pageone/index.vue
+++ pages/pageone/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="container">
-    <h1>This is pageone 'pages' thing.</h1>
+    <h1>
+      This is pageone 'pages' thing.
+    </h1>
   </div>
 </template>
-
 <script>
 export default {}
 </script>
 
-<style lang="sass" scoped>
-
-</style>
-
+<style lang="sass" scoped></style>
