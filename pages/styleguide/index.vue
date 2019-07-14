2e5c1fe21f331ac8e8d57e24c8efe09d8aed9e0f
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2019-07-14 10:15:34 -0500

    Getting familiar with ScrollMagic.

--- pages/styleguide/index.vue
+++ pages/styleguide/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <section class="styleguide segment">
+    <section id="bar" class="styleguide segment">
       <div class="container container--narrow text">
-        <div id="target" class="indicator"></div>
+        <div id="foo" class="indicator"></div>
         <h1>
           Fusce fringilla nisl sed eros finibus volutpat. Praesent convallis
           elit non sapien suscipit, quis pellentesque ligula egestas.
@@ -44,9 +44,53 @@
         </p>
       </div>
     </section>
-    <section class="styleguide segment segment--dark">
+    <section id="bing" class="styleguide segment segment--dark">
       <div class="container container--narrow text">
-        <div class="indicator"></div>
+        <div id="bong" class="indicator"></div>
+        <h1>
+          Fusce fringilla nisl sed eros finibus volutpat. Praesent convallis
+          elit non sapien suscipit, quis pellentesque ligula egestas.
+        </h1>
+        <h2>
+          Nullam aliquam erat nec ipsum accumsan euismod. Donec rhoncus orci non
+          cursus porttitor.
+        </h2>
+        <h3>
+          Phasellus ut nunc quis purus sagittis sagittis. Nam quis metus
+          blandit, mollis neque sed, dignissim lectus. Sed viverra nibh quis
+          sagittis maximus. Duis ac ligula eget ex sollicitudin aliquam sit amet
+          in risus.
+        </h3>
+        <h4>
+          Nulla lacinia risus a mattis mattis. Mauris elementum felis in
+          sollicitudin dictum. Ut rhoncus neque ut volutpat aliquam. Donec id
+          leo pretium, auctor urna vitae, pharetra mauris. Fusce sit amet dolor
+          ut purus mollis tempor id eget nibh.
+        </h4>
+        <h5>
+          Suspendisse sollicitudin diam et arcu viverra convallis. Fusce eu quam
+          vitae est tempus porttitor. Maecenas volutpat urna sed nunc iaculis
+          sagittis. Donec lacinia nisi maximus, mattis odio viverra, rhoncus
+          metus.
+        </h5>
+        <p>
+          Proin ultrices nibh eget condimentum facilisis. Nunc in lorem et mi
+          facilisis vulputate. <strong>Morbi porta pellentesque</strong> nibh
+          scelerisque auctor. Nunc enim lacus, ultricies in pharetra a,
+          porttitor vel dui. Duis tincidunt rutrum lorem et porta. Nunc sit amet
+          porta erat. Vivamus vel diam a neque dictum feugiat. Mauris ultrices
+          scelerisque turpis sed porta. Nunc nec tempor massa.
+        </p>
+        <p>
+          Proin auctor nisl mattis lorem varius mattis. Aliquam cursus dui non
+          ullamcorper volutpat. Suspendisse quis dui in ante maximus fermentum
+          ac vitae risus.
+        </p>
+      </div>
+    </section>
+    <section id="bar" class="styleguide segment">
+      <div class="container container--narrow text">
+        <div id="foo" class="indicator"></div>
         <h1>
           Fusce fringilla nisl sed eros finibus volutpat. Praesent convallis
           elit non sapien suscipit, quis pellentesque ligula egestas.
@@ -112,13 +156,18 @@ export default {
     /* eslint-enable */
   },
   mounted: function() {
-    const controller = new this.$ScrollMagic.Controller()
-    const scene = new this.$ScrollMagic.Scene({
-      offset: 100, // start scene after scrolling for 100px
-      duration: 400 // pin the element for 400px of scrolling
-    }).setPin('#target')
-    controller.addScene(scene)
-    console.log('mounted')
+    const ScrollMagic = this.$ScrollMagic
+    const elements = document.querySelectorAll('.segment')
+    Array.from(elements).forEach(function(element) {
+      const controller = new ScrollMagic.Controller({
+        globalSceneOptions: { duration: element.offsetHeight }
+      })
+      console.log(element.offsetHeight)
+      new ScrollMagic.Scene({ triggerElement: element, triggerHook: 0.5 })
+        .setClassToggle(element.querySelector('.indicator'), 'active') // add class toggle
+        .addTo(controller)
+      console.log(element)
+    })
   }
 }
 </script>
@@ -132,7 +181,11 @@ export default {
   height: 100%;
   width: 5px;
   background-color: orange;
+  transition: background-color 1s;
 
+  &.active {
+    background-color: rebeccapurple !important;
+  }
   .segment--dark & {
     background-color: crimson;
   }
