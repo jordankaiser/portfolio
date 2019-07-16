716d643738014b34b64171c2087959bc90cffc34
Jordan Kaiser <jordan.ross.kaiser@gmail.com>
2019-07-16 11:00:12 -0500

    Adjusted how we're pulling in greensock imports. The previous way was working when run npm run dev but not in production mode. These changes fix that. The bundle size is still smaller then it was when the refactoring started.

--- plugins/gsap-scrollmagic.js
+++ plugins/gsap-scrollmagic.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
 import Vue from 'vue'
-import '~gsap/TweenLite';
-import '~gsap/TimelineLite';
-import '~gsap/plugins/CSSPlugin';
+// import '~gsap/TweenLite';
+// import '~gsap/TimelineLite';
+// import '~gsap/plugins/CSSPlugin';
+import {TweenLite, CSSPlugin, TimelineLite} from "gsap/all";
 
 import 'script-loader!~ScrollMagic';
 import 'script-loader!~animation.gsap';
