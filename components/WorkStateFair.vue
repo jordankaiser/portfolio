8c9d59f1a278280fea95172e72aa186322ad3ad2
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2019-11-18 10:25:53 -0600

    Changes:
    - Produced corn artwork.
    - Added in corn artowrk styling for mobile.

--- components/WorkStateFair.vue
+++ components/WorkStateFair.vue
@@ -1,22 +1,29 @@
 <template>
   <section class="work segment">
-    <div class="work__container container">
-      <div class="work__intro">
-        <div class="work__text">
-          <p class="work__subhead">Development</p>
-          <h2 class="work__heading">Nebraska State Fair</h2>
-          <p class="work__description">
-            Brief description of project amet nulla vel nunc placerat ultricies
-            eget vel massa. Ut efficitur ex ut libero vestibulum porta.
-            Vestibulum sed purus aliquet, consequat.
-          </p>
-        </div>
-        <div class="work__illustration"></div>
+    <div class="work__intro">
+      <div class="work__text">
+        <p class="work__subhead">Development</p>
+        <h2 class="work__heading">Nebraska State Fair</h2>
+      </div>
+      <div class="work__illustration">
+        <corn-hero />
       </div>
     </div>
+    <p class="container work__description">
+      Brief description of project amet nulla vel nunc placerat ultricies eget
+      vel massa. Ut efficitur ex ut libero vestibulum porta. Vestibulum sed
+      purus aliquet, consequat.
+    </p>
   </section>
 </template>
-
+<script>
+import CornHero from '~/components/work-illustrations/CornHero.vue'
+export default {
+  components: {
+    CornHero
+  }
+}
+</script>
 <style lang="scss" scoped>
 @import '~/assets/scss/_variables.scss';
 @import '~/assets/scss/_mixins.scss';
@@ -34,20 +41,20 @@
   &__intro {
     display: flex;
     justify-content: space-between;
+    padding-left: $s0;
   }
   &__text {
     flex: 0 1 auto;
+    align-self: flex-end;
+    margin-bottom: 35px;
     width: 100%;
   }
   &__heading {
     margin-top: 9px;
   }
-  &__description {
-    margin-top: 35px;
-  }
   &__illustration {
     flex: 0 0 auto;
-    width: 90px;
+    width: 114px;
   }
 }
 </style>
---
+++ components/work-illustrations/CornHero.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="corn-hero">
+    <img
+      src="~/assets/img/corn-1.png"
+      alt="Corn Illustration"
+      class="corn-hero__1"
+    />
+    <img
+      src="~/assets/img/corn-2.png"
+      alt="Corn Illustration"
+      class="corn-hero__2"
+    />
+    <img
+      src="~/assets/img/corn-3.png"
+      alt="Corn Illustration"
+      class="corn-hero__3"
+    />
+    <div class="corn-hero__circle"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '~/assets/scss/_variables.scss';
+.corn-hero {
+  position: relative;
+  overflow: hidden;
+  padding-bottom: 10px;
+
+  &__1 {
+    position: relative;
+    width: 161px;
+    height: auto;
+    z-index: 1;
+  }
+  &__2 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 161px;
+    height: auto;
+    z-index: 1;
+  }
+  &__3 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 161px;
+    height: auto;
+    z-index: 1;
+  }
+  &__circle {
+    position: absolute;
+    width: 140px;
+    height: 140px;
+    top: 55%;
+    left: 99%;
+    transform: translate(-50%, -50%);
+    background-color: $color-orange;
+    border-radius: 100%;
+  }
+}
+</style>
