b2a764b4dd6966c75855c2ca0347454b451a6d92
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2020-03-15 11:03:07 -0500

    progress on work detail tooling section etc

--- components/works/mc/Screenshot.vue
+++ components/works/mc/Screenshot.vue
@@ -44,7 +44,7 @@ export default {
     max-width: 344px;
     width: 100%;
     height: auto;
-    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.42);
+    box-shadow: $box-shadow;
     border-top-left-radius: 10px;
     border-bottom-left-radius: 10px;
 
--- pages/memorialcare/index.vue
+++ pages/memorialcare/index.vue
@@ -3,32 +3,40 @@
     <section class="work-details segment">
       <div class="work-details__sub-segment">
         <div class="introduction container container--narrow">
-          <h4 class="introduction__roles">Roles <span>|</span> Development</h4>
-          <h1 class="introduction__title">MemorialCare</h1>
-          <div class="introduction__description">
-            <p>
-              Introduction of work donec maximus turpis eget quam elementum
-              laoreet. Quisque euismod leo a magna congue, vitae scelerisque
-              eros dapibus. Mauris in quam vitae tortor consectetur volutpat at
-              id purus. Maecenas maximus nibh quis mauris molestie, at lacinia
-              arcu finibus. Mauris at sem at metus luctus mattis vel ac massa.
-            </p>
+          <div class="introduction">
+            <h4 class="introduction__roles">
+              Roles <span>|</span> Development
+            </h4>
+            <h1 class="introduction__title">MemorialCare</h1>
+            <div class="introduction__description">
+              <p>
+                Introduction of work donec maximus turpis eget quam elementum
+                laoreet. Quisque euismod leo a magna congue, vitae scelerisque
+                eros dapibus. Mauris in quam vitae tortor consectetur volutpat
+                at id purus. Maecenas maximus nibh quis mauris molestie, at
+                lacinia arcu finibus. Mauris at sem at metus luctus mattis vel
+                ac massa.
+              </p>
+            </div>
           </div>
         </div>
       </div>
       <div class="work-details__sub-segment">
-        <div class="tools container container--narrow">
-          <div class="tools__list">
-            <ul>
-              <li>Drupal</li>
-              <li>PHP</li>
-              <li>Javascript</li>
-              <li>Vue.js</li>
-              <li>Twig</li>
-              <li>SCSS</li>
-            </ul>
+        <div class="container container--narrow">
+          <div class="tools">
+            <div class="tools__listing">
+              <h4 class="tools__list-title">Tools</h4>
+              <ul class="tools__list">
+                <li class="tools__list-item">Drupal</li>
+                <li class="tools__list-item">PHP</li>
+                <li class="tools__list-item">Javascript</li>
+                <li class="tools__list-item">Vue.js</li>
+                <li class="tools__list-item">Twig</li>
+                <li class="tools__list-item">SCSS</li>
+              </ul>
+            </div>
+            <div class="tools__illustration"></div>
           </div>
-          <div class="tools__illustration"></div>
         </div>
       </div>
     </section>
@@ -75,11 +83,57 @@ export default {
 .tools {
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  padding-top: 15px;
+  padding-left: 15px;
+  padding-bottom: 20px;
+  padding-right: 15px;
+  border-radius: 10px;
+  box-shadow: $box-shadow;
 
-  &__list {
+  &__listing {
     flex: 0 1 auto;
     width: 100%;
-    background-color: lightcoral;
+  }
+  &__list {
+    display: flex;
+    flex-direction: column;
+    margin-top: 13px;
+
+    @include breakpoint($small) {
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
+  }
+  &__list-title {
+    font-size: 13px;
+    text-transform: uppercase;
+  }
+  &__list-item {
+    flex: 0 1 auto;
+    position: relative;
+    padding-left: 15px;
+    margin-top: $s0;
+
+    @include breakpoint($small) {
+      width: 150px;
+
+      &:nth-child(2) {
+        margin-top: 0;
+      }
+    }
+    &:nth-child(1) {
+      margin-top: 0;
+    }
+    &:before {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: -1px;
+      width: 5px;
+      height: 20px;
+      background-color: $color-mc-pink;
+    }
   }
   &__illustration {
     width: 161px;
