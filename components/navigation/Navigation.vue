e81bb73718d77a73a539c658d790475bf3167fb5
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2020-10-22 08:07:28 -0500

    Progress on nav element

--- components/navigation/Navigation.vue
+++ components/navigation/Navigation.vue
@@ -1,11 +1,36 @@
 <template>
-  <nav class="menu">nav</nav>
+  <nav class="menu">
+    <button class="hamburger" aria-label="menu" aria-expanded="false">
+      <div class="hamburger__line hamburger__line--one"></div>
+      <div class="hamburger__line hamburger__line--two"></div>
+      <div class="hamburger__line hamburger__line--three"></div>
+    </button>
+  </nav>
 </template>
 <style lang="scss" scoped>
+@import '~/assets/scss/_mixins.scss';
+@import '~/assets/scss/_variables.scss';
 .menu {
   position: fixed;
   top: 0;
   right: 0;
-  z-index: 1;
+  z-index: 6;
+}
+.hamburger {
+  @include buttonReset;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 12px;
+  height: 48px;
+  background-color: $color-red;
+  border-bottom-left-radius: 10px;
+
+  &__line {
+    width: 32px;
+    height: 4px;
+    background-color: $color-white;
+    border-radius: 10px;
+  }
 }
 </style>
