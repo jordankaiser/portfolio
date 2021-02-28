a28c173026433a4efbdae0140263f79cc2428ea8
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2021-02-28 08:10:37 -0600

    Progress on change work detail pages to use a template component.

--- components/work-details/WorkDetail.vue
+++ components/work-details/WorkDetail.vue
@@ -18,15 +18,9 @@
               </span>
             </span>
           </h4>
-          <h1 class="introduction__title container">MemorialCare</h1>
+          <h1 class="introduction__title container">{{ workDetails.title }}</h1>
           <div class="introduction__description container container--narrow">
-            <p>
-              Introduction of work donec maximus turpis eget quam elementum
-              laoreet. Quisque euismod leo a magna congue, vitae scelerisque
-              eros dapibus. Mauris in quam vitae tortor consectetur volutpat at
-              id purus. Maecenas maximus nibh quis mauris molestie, at lacinia
-              arcu finibus. Mauris at sem at metus luctus mattis vel ac massa.
-            </p>
+            <p>{{ workDetails.description }}</p>
           </div>
         </div>
       </div>
@@ -34,47 +28,39 @@
         <div class="screenshots">
           <div class="screenshots__screenshot">
             <!-- Slot for Screenshot.vue -->
-            <Screenshot :screenshot="screenshotOne">
+            <Screenshot :screenshot="workDetails.screenshotOne">
               <template v-slot:caption>
                 <!-- Slot for ScreenshotCaption.vue -->
-                <ScreenshotCaption :caption="screenshotOneCaption">
+                <ScreenshotCaption :caption="workDetails.screenshotOneCaption">
                   <template v-slot:captionText>
-                    <p>
-                      The Cash Pricing tool allows the user to specifiy a
-                      medical procedure and medical facility. The cash price of
-                      that procedure is then given.
-                    </p>
-                    <p>
-                      This tool was built with Vue.js and utilizes Vuex for
-                      state management.
-                    </p>
+                    <div v-html="workDetails.screenshotOneCaptionText"></div>
                   </template>
                 </ScreenshotCaption>
               </template>
               <template v-slot:cta>
-                <CtaSecondary :link="screenshotOneLink" :varient="id" />
+                <CtaSecondary
+                  :link="workDetails.screenshotOneLink"
+                  :varient="workDetails.id"
+                />
               </template>
             </Screenshot>
           </div>
           <div class="screenshots__screenshot">
             <!-- Slot for Screenshot.vue -->
-            <Screenshot :screenshot="screenshotTwo">
+            <Screenshot :screenshot="workDetails.screenshotTwo">
               <template v-slot:caption>
                 <!-- Slot for ScreenshotCaption.vue -->
-                <ScreenshotCaption :caption="screenshotTwoCaption">
+                <ScreenshotCaption :caption="workDetails.screenshotTwoCaption">
                   <template v-slot:captionText>
-                    <p>
-                      One of the objectives for the project was allow the user
-                      to specify a particular medical location. Once chosen the
-                      user sees specific content for that location throughout
-                      the site. The Cancer Care service page is one example of
-                      this.
-                    </p>
+                    <div v-html="workDetails.screenshotTwoCaptionText"></div>
                   </template>
                 </ScreenshotCaption>
               </template>
               <template v-slot:cta>
-                <CtaSecondary :link="screenshotTwoLink" :varient="id" />
+                <CtaSecondary
+                  :link="workDetails.screenshotTwoLink"
+                  :varient="workDetails.id"
+                />
               </template>
             </Screenshot>
           </div>
@@ -145,9 +131,61 @@ export default {
       default: function() {
         return {}
       },
+      id: {
+        type: String,
+        default: 'mc'
+      },
       roles: {
         type: Array,
         default: ['Developer']
+      },
+      title: {
+        type: String,
+        default: 'Work Title'
+      },
+      description: {
+        type: String,
+        default: 'Laoreet class eleifend dis.'
+      },
+      screenshotOne: {
+        type: Object,
+        default: function() {
+          return {
+            id: 'one',
+            image: 'mc/work-cash-pricing.jpg',
+            alt: 'MemorialCare Home Page',
+            placeholderConfig: {
+              initial: null,
+              width: '1000',
+              height: '1000'
+            },
+            animationConfig: {
+              id: 'mc-screenshot-one',
+              revealOffset: 0.75,
+              initialDelay: '+=0',
+              triggerEl: '.screenshot--one'
+            }
+          }
+        }
+      },
+      screenshotOneCaption: {
+        id: 'mc',
+        uniqueId: 'one',
+        cta: '.screenshot--one .cta-secondary'
+      },
+      screenshotOneCaptionText: {
+        type: String,
+        default: 'Caption text'
+      },
+      screenshotOneLink: {
+        type: Object,
+        default: function() {
+          return {
+            text: 'Link Title',
+            href: '/',
+            target: '_self'
+          }
+        }
       }
     }
   },
@@ -156,7 +194,6 @@ export default {
   },
   data: function() {
     return {
-      id: 'mc',
       headerModifiers: {
         id: 'mc-header',
         revealOffset: 0.7
@@ -166,57 +203,6 @@ export default {
         revealOffset: 0.8,
         initialDelay: '+=1',
         triggerEl: '.mc-hero'
-      },
-      screenshotOneLink: {
-        text: 'Cash Pricing',
-        href: 'https://memorialcare.org/cash-pricing',
-        target: '_blank'
-      },
-      screenshotTwoLink: {
-        text: 'Cancer Care Service',
-        href: 'https://www.memorialcare.org/services/cancer-care',
-        target: '_blank'
-      },
-      screenshotOne: {
-        id: 'one',
-        image: 'mc/work-cash-pricing.jpg',
-        alt: 'MemorialCare Home Page',
-        placeholderConfig: {
-          initial: null,
-          width: '1000',
-          height: '1000'
-        },
-        animationConfig: {
-          id: 'mc-screenshot-one',
-          revealOffset: 0.75,
-          initialDelay: '+=0',
-          triggerEl: '.screenshot--one'
-        }
-      },
-      screenshotTwo: {
-        id: 'two',
-        image: 'mc/work-small-service-line.jpg',
-        placeholderConfig: {
-          initial: null,
-          width: '582',
-          height: '570'
-        },
-        animationConfig: {
-          id: 'mc-screenshot-two',
-          revealOffset: 0.75,
-          initialDelay: '+=0',
-          triggerEl: '.screenshot--two'
-        }
-      },
-      screenshotOneCaption: {
-        id: 'mc',
-        uniqueId: 'one',
-        cta: '.screenshot--one .cta-secondary'
-      },
-      screenshotTwoCaption: {
-        id: 'mc',
-        uniqueId: 'two',
-        cta: '.screenshot--two .cta-secondary'
       },
       ctaHero: {
         id: 'mc',
--- pages/memorialcare/index.vue
+++ pages/memorialcare/index.vue
@@ -11,7 +11,66 @@ export default {
   data: function() {
     return {
       workDetails: {
-        roles: ['Developer', 'Another']
+        id: 'mc',
+        roles: ['Developer', 'Another'],
+        title: 'MemorialCare',
+        description:
+          'Introduction of work tortor ultricies malesuada viverra leo amet etiam feugiat senectus curabitur in felis himenaeos, a potenti libero eget pretium donec conubia nunc netus litora faucibus.',
+        screenshotOne: {
+          id: 'one',
+          image: 'mc/work-cash-pricing.jpg',
+          alt: 'MemorialCare Home Page',
+          placeholderConfig: {
+            initial: null,
+            width: '1000',
+            height: '1000'
+          },
+          animationConfig: {
+            id: 'mc-screenshot-one',
+            revealOffset: 0.75,
+            initialDelay: '+=0',
+            triggerEl: '.screenshot--one'
+          }
+        },
+        screenshotOneCaption: {
+          id: 'mc',
+          uniqueId: 'one',
+          cta: '.screenshot--one .cta-secondary'
+        },
+        screenshotOneCaptionText:
+          '<p>The Cash Pricing tool allows the user to specifiy a medical procedure and medical facility. The cash price of that procedure is then given.</p><p>This tool was built with Vue.js and utilizes Vuex for state management.</p>',
+        screenshotOneLink: {
+          text: 'Cash Pricing',
+          href: 'https://memorialcare.org/cash-pricing',
+          target: '_blank'
+        },
+        screenshotTwo: {
+          id: 'two',
+          image: 'mc/work-small-service-line.jpg',
+          placeholderConfig: {
+            initial: null,
+            width: '582',
+            height: '570'
+          },
+          animationConfig: {
+            id: 'mc-screenshot-two',
+            revealOffset: 0.75,
+            initialDelay: '+=0',
+            triggerEl: '.screenshot--two'
+          }
+        },
+        screenshotTwoCaption: {
+          id: 'mc',
+          uniqueId: 'two',
+          cta: '.screenshot--two .cta-secondary'
+        },
+        screenshotTwoCaptionText:
+          '<p>One of the objectives for the project was allow the user to specify a particular medical location. Once chosen the user sees specific content for that location throughout the site. The Cancer Care service page is one example of this.</p>',
+        screenshotTwoLink: {
+          text: 'Cancer Care Service',
+          href: 'https://www.memorialcare.org/services/cancer-care',
+          target: '_blank'
+        }
       }
     }
   }
